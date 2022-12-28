0dde2c3df64fe8db50449188867285a5d1a8a3f6
fabianomalves <fabianomalves@proton.me>
2022-12-28 17:17:24 -0300

    Create new sql queries

--- select_questions.sql
+++ select_questions.sql
@@ -12,6 +12,7 @@ SELECT
 FROM begrowth_user_api_demo_bg_users_bg_data_enginner_test_fabiano 
 GROUP BY address_state 
 ORDER BY COUNT(DISTINCT ID) DESC;
+-----------------------------
 
 /*
  * Quantos usuários únicos temos por estado
@@ -25,6 +26,7 @@ SELECT
 FROM begrowth_user_api_demo_bg_users_bg_data_enginner_test_fabiano 
 GROUP BY address_state 
 HAVING COUNT(DISTINCT ID) <= '1';
+-----------------------------
 
 /* Quantos usuários temos por source 
  * ordando por ordem alfabética?
@@ -37,7 +39,7 @@ SELECT
 FROM begrowth_user_api_demo_bg_users_bg_data_enginner_test_fabiano
 GROUP BY utm_source
 ORDER BY utm_source ASC;
-
+-----------------------------
 
 
 
@@ -51,6 +53,8 @@ SELECT
 FROM begrowth_user_api_demo_bg_users_bg_data_enginner_test_fabiano 
 GROUP BY utm_source
 ORDER BY utm_source ASC;
+-----------------------------
+
 
 
 /*
@@ -59,24 +63,72 @@ ORDER BY utm_source ASC;
  * (Criar flag same_state no select) 
  */
 
--- a coluna UF da tabela begrowth_user_api_demo_bg_users_brazilian_state
--- e a coluna address_state da tabela begrowth_user_api_demo_bg_users_bg_data_enginner_test_fabiano
--- estão diferentes. Olhar uma forma de fazer um de para com elas
+
+SELECT 
+    t1.cpf AS cpf_user,
+    t1.address_state AS adress_state_user_acronym,
+    t2.initials AS brazilian_state_acronym,
+    t3.id AS cpf_state_ids,
+    t3.uf AS region_state_cpf 
+FROM begrowth_user_api_demo_bg_users_bg_data_enginner_test_fabiano AS t1
+LEFT JOIN begrowth_user_api_demo_bg_users_brazilian_state AS t2
+ON t1.address_state = t2.initials 
+LEFT JOIN begrowth_user_api_demo_bg_users_cpf_state AS t3
+ON SUBSTRING(t1.cpf, 9, 1) = CAST (t3.id AS TEXT)  
+-----------------------------
+
+
+SELECT 
+    t1.cpf AS cpf_user,
+    t1.address_state AS adress_state_user_acronym,
+    t2.initials AS brazilian_state_acronym,
+    t3.id AS cpf_state_ids,
+    t3.uf AS region_state_cpf,
+    COALESCE(
+        (
+            SELECT 'same_state'
+            FROM (
+                SELECT 
+                    t1.cpf AS cpf_user,
+                    t1.address_state AS adress_state_user_acronym,
+                    t2.initials AS brazilian_state_acronym,
+                    t3.id AS cpf_state_ids,
+                    t3.uf AS region_state_cpf 
+                FROM begrowth_user_api_demo_bg_users_bg_data_enginner_test_fabiano AS t1
+                LEFT JOIN begrowth_user_api_demo_bg_users_brazilian_state AS t2
+                ON t1.address_state = t2.initials 
+                LEFT JOIN begrowth_user_api_demo_bg_users_cpf_state AS t3
+                ON SUBSTRING(t1.cpf, 9, 1) = CAST (t3.id AS TEXT)
+            ) AS subquery
+            WHERE region_state_cpf = t2.initials
+            LIMIT 1
+        ), 'different_state'
+    ) AS same_state
+FROM begrowth_user_api_demo_bg_users_bg_data_enginner_test_fabiano AS t1
+LEFT JOIN begrowth_user_api_demo_bg_users_brazilian_state AS t2
+ON t1.address_state = t2.initials 
+LEFT JOIN begrowth_user_api_demo_bg_users_cpf_state AS t3
+ON SUBSTRING(t1.cpf, 9, 1) = CAST (t3.id AS TEXT);
+
+
+-----------------------------
+
 
 SELECT *
 FROM begrowth_user_api_demo_bg_users_bg_data_enginner_test_fabiano 
 LIMIT 10;
+-----------------------------
 
 SELECT  
     *
 FROM begrowth_user_api_demo_bg_users_brazilian_state
+-----------------------------
 
-SELECT
-    t1.id,
-    t1.address_state,
-    t2.uf 
-FROM begrowth_user_api_demo_bg_users_bg_data_enginner_test_fabiano AS t1
-LEFT JOIN begrowth_user_api_demo_bg_users_brazilian_state AS t2
-ON t1.address_state = t2.uf 
+SELECT  
+    *
+FROM begrowth_user_api_demo_bg_users_cpf_state
+-----------------------------
+
+
  
 
